d41fca522b1f6a811ccdb9561e0c2f3b3e5e29b1
mhbabu <write2babu.cse@gmail.com>
2026-03-31 10:57:29 +0600

    Constants folder data set done

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -8,6 +8,13 @@ class TColors {
   static const Color secondary = Color(0xFFFFE24B);
   static const Color accent = Color(0xFFB0C7FF);
 
+  // Gradient Colors
+  static const Gradient linerGradient = LinearGradient(
+    begin: Alignment(0.0, 0.0),
+    end: Alignment(0.707, -0.707),
+    colors: [Color(0xfffff9a9e), Color(0xfffad0c4), Color(0xfffad0c4)],
+  ); // LinearGradient
+
   // ================== Text Colors ==================
   static const Color textPrimary = Color(0xFF333333);
   static const Color textSecondary = Color(0xFF6C757D);
