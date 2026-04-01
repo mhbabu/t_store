b31b713ff20530286a877599951210185d6c899e
mhbabu <write2babu.cse@gmail.com>
2026-04-01 17:51:17 +0600

    PageDot design done

--- lib/features/authentication/screens/onboarding.dart
+++ lib/features/authentication/screens/onboarding.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:t_store/features/authentication/screens/onboarding/widgets/onboarding_page.dart';
+import 'package:t_store/features/authentication/screens/onboarding/widgets/onboarding_skip.dart';
+import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/constants/image_string.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/constants/text_string.dart';
-import 'package:t_store/utils/helpers/helper_functions.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:t_store/utils/device/device_utility.dart';
 
 class OnBoardingScreen extends StatelessWidget {
@@ -13,48 +16,44 @@ class OnBoardingScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
+          //Horizontal Scrollable Pages
           PageView(
             children: [
-              OnBoardingPage(image: TImages.onBoardingImage1, title: TTexts.onBoardingTitle1, subtitle: TTexts.onBoardingSubTitle1),
-              OnBoardingPage(image: TImages.onBoardingImage2, title: TTexts.onBoardingTitle2, subtitle: TTexts.onBoardingSubTitle2),
-              OnBoardingPage(image: TImages.onBoardingImage3, title: TTexts.onBoardingTitle3, subtitle: TTexts.onBoardingSubTitle3),
+              OnBoardingPage(
+                image: TImages.onBoardingImage1,
+                title: TTexts.onBoardingTitle1,
+                subtitle: TTexts.onBoardingSubTitle1,
+              ),
+              OnBoardingPage(
+                image: TImages.onBoardingImage2,
+                title: TTexts.onBoardingTitle2,
+                subtitle: TTexts.onBoardingSubTitle2,
+              ),
+              OnBoardingPage(
+                image: TImages.onBoardingImage3,
+                title: TTexts.onBoardingTitle3,
+                subtitle: TTexts.onBoardingSubTitle3,
+              ),
             ],
           ),
 
-          OnBoardingSkip()
-        ],
-      ),
-    );
-  }
-}
+          //Skip Button
+          const OnBoardingSkip(),
 
+          // Dot Nagator Smooth Page Indicator
+          Positioned(
+            bottom: TDeviceUtils.getBottomNavigationBarHeight() + 25,
+            left: TSizes.defaultSpace,
 
-class OnBoardingSkip extends StatelessWidget {
-  const OnBoardingSkip({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Positioned(top: TDeviceUtils.getAppBarHeight(), right: TSizes.defaultSpace, child: TextButton(onPressed: (){}, child: const Text("Skip")), );
-  }
-}
-
-class OnBoardingPage extends StatelessWidget {
-  const OnBoardingPage({super.key, required this.image, required this.title, required this.subtitle});
-
-final String image, title, subtitle;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(TSizes.defaultSpace),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image(width: THelperFunctions.screenWidth() * 0.8, height: THelperFunctions.screenHeight() * 0.6, image: AssetImage(image)),
-          Text(title, style: Theme.of(context).textTheme.headlineMedium, textAlign: TextAlign.center),
-          const SizedBox(height: TSizes.spaceBtwItems),
-          Text(subtitle, style: Theme.of(context).textTheme.bodyMedium, textAlign: TextAlign.center),
+            child: SmoothPageIndicator(
+              controller: PageController(),
+              count: 3,
+              effect: ExpandingDotsEffect(
+                activeDotColor: TColors.dark,
+                dotHeight: 6,
+              ),
+            ),
+          ),
         ],
       ),
     );
